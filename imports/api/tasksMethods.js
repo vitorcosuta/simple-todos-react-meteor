14b2c54e294ea009cb67668310b734e4a9d6856a
Vitor Costa <vitorcostamgbr@gmail.com>
2025-04-16 12:31:02 -0300

    fix(api): incluir campos de cadastro de tarefa

--- imports/api/tasksMethods.js
+++ imports/api/tasksMethods.js
@@ -4,34 +4,34 @@ import { TasksCollection } from "./TasksCollection";
 const INITIAL_TASK_STATUS = 'Cadastrada';
 
 Meteor.methods({
-  "tasks.insert"(doc) {
-    return TasksCollection.insertAsync({
-      ...doc,
-      userId: this.userId,
-      status: INITIAL_TASK_STATUS,
-      isPersonal: false,
-    });
-  },
-  "tasks.changeStatus"({ _id, status }) {
-    return TasksCollection.updateAsync(_id, {
-      $set: { status },
-    });
-  },
-  "tasks.changePrivacy"({ _id, isPersonal }) {
-    return TasksCollection.updateAsync(_id, {
-      $set: { isPersonal: !isPersonal },
-    });
-  },
-  "tasks.delete"({ _id }) {
-    return TasksCollection.removeAsync(_id);
-  },
-  "tasks.update"({ _id, newText, newDescription, newDate }) {
-    return TasksCollection.updateAsync(_id, {
-      $set: { 
-        text: newText,
-        description: newDescription,
-        date: newDate,
-      }
-    });
-  },
+    "tasks.insert"(doc) {
+        return TasksCollection.insertAsync({
+            ...doc,
+            userId: this.userId,
+            status: INITIAL_TASK_STATUS,
+            isPersonal: false,
+        });
+    },
+    "tasks.changeStatus"({ _id, status }) {
+        return TasksCollection.updateAsync(_id, {
+            $set: { status },
+        });
+    },
+    "tasks.changePrivacy"({ _id, isPersonal }) {
+        return TasksCollection.updateAsync(_id, {
+            $set: { isPersonal: !isPersonal },
+        });
+    },
+    "tasks.delete"({ _id }) {
+        return TasksCollection.removeAsync(_id);
+    },
+    "tasks.update"({ _id, newText, newDescription, newDate }) {
+        return TasksCollection.updateAsync(_id, {
+            $set: { 
+            name: newText,
+            description: newDescription,
+            date: newDate,
+            }
+        });
+    },
 });
--- server/main.js
+++ server/main.js
@@ -9,47 +9,47 @@ import "../imports/api/usersMethods";
 
 const INITIAL_TASK_STATUS = 'Cadastrada';
 
-const insertTask = (taskText, user) =>
-  TasksCollection.insertAsync({
-    text: taskText,
+const insertTask = (taskName, user) =>
+    TasksCollection.insertAsync({
+    name: taskName,
     userId: user._id,
     createdAt: new Date(),
     status: INITIAL_TASK_STATUS,
     isPersonal: false,
-  });
+});
 
 const SEED_EMAIL = 'admin@exemplo.com';
 const SEED_PASSWORD = 'senha123';
 
 Meteor.startup(async () => {
 
-  const existingUser = await Accounts.findUserByEmail(SEED_EMAIL)
+    const existingUser = await Accounts.findUserByEmail(SEED_EMAIL)
 
-  if (!existingUser) {
-    Accounts.createUser({
-      email: SEED_EMAIL,
-      password: SEED_PASSWORD,
-      profile: {
-        name: 'Administrador',
-        company: 'Minha Empresa',
-        gender: 'Outro',
-        birthdate: new Date('1990-01-01'),
-        photo: defaultUserIcon
-      }
-    });
-  }
+    if (!existingUser) {
+        Accounts.createUser({
+            email: SEED_EMAIL,
+            password: SEED_PASSWORD,
+            profile: {
+            name: 'Administrador',
+            company: 'Minha Empresa',
+            gender: 'Outro',
+            birthdate: new Date('1990-01-01'),
+            photo: defaultUserIcon
+            }
+        });
+    }
 
-  const user = await Accounts.findUserByEmail(SEED_EMAIL);
+    const user = await Accounts.findUserByEmail(SEED_EMAIL);
 
-  if ((await TasksCollection.find().countAsync()) === 0){
+    if ((await TasksCollection.find().countAsync()) === 0){
     [
-      "First Task",
-      "Second Task",
-      "Third Task",
-      "Fourth Task",
-      "Fifth Task",
-      "Sixth Task",
-      "Seventh Task",
+        "First Task",
+        "Second Task",
+        "Third Task",
+        "Fourth Task",
+        "Fifth Task",
+        "Sixth Task",
+        "Seventh Task",
     ].forEach((taskText) => insertTask(taskText, user));
-  }
+    }
 });
